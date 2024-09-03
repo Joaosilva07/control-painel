c603d1c11430f98caba3aa215262c0118fff45e7
Joaosilva07 <joao.silva07@sempreceub.com>
2024-09-03 09:54:01 -0300

    Added date and hour in dialog

--- js/dialog.js
+++ js/dialog.js
@@ -1,11 +1,21 @@
 const dialogPoint = document.getElementById('dialog-point');
 const openDialogButton = document.getElementById('home-page-button');
 const closeDialogButton = document.getElementById('close-dialog-button');
+const dialogD = document.getElementById('dialog-date');
+const dialogH= document.getElementById('dialog-clock');
 
 openDialogButton.addEventListener('click', () => {
-    dialogPoint.style.display = 'block';
+    dialogD.textContent = getCurrentDate();
+    dialogH.textContent = updateDialogClock();
+    dialogPoint.showModal();
 });
 
 closeDialogButton.addEventListener('click', () => {
-    dialogPoint.style.display = 'none';
+    dialogPoint.close();
 });
+
+function updateDialogClock(){
+    dialogH.textContent = getCurrentTime();
+}
+
+setInterval(updateDialogClock, 1000);
